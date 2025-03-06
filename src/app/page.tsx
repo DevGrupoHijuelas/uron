6346416cf4682e8e43ba0111a6b3de8fc07956c9
Ghost0192 <mcastillo@grupohijuelas.cl>
2025-03-06 10:31:14 -0300

    thema
    se actualizo el tema a black

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900 text-center p-6">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-center p-6">
       {/* Logo */}
       <Image
         src="/img/lumina green a.webp" // Cambia esto por la ruta de tu logo
